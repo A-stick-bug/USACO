d905546a65bd8e493e092cedca0e84c6626c5473
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-16 22:08:15 -0400

    added explanation

--- 2016_january_silver_q2_subsequences_summing_to_seven.py
+++ 2016_january_silver_q2_subsequences_summing_to_seven.py
@@ -11,6 +11,8 @@ for i in range(1, n):
     num = int(input())
     arr.append((num + arr[-1]) % 7)
 
+# difference (index) of the first and last occurrence of 'i' in 'arr' is the most cows you can fit in
+# because if 2 numbers have the same mod%7, one number minus the other will result in a multiple of 7
 res = 0
 for i in range(7):
     if i not in arr:
